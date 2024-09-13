45b36f0049be7c678ef71917d7c09f830de6c577
mashaler <mashalerolva@gmail.com>
2024-09-13 20:55:16 +0200

    connected static folders

--- studentstudyportal/settings.py
+++ studentstudyportal/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static'
+STATICFILES_DIRS = [BASE_DIR/"static"]
