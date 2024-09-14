9cb8204124c03b76ca13485662e7cf3f7417b067
mashaler <mashalerolva@gmail.com>
2024-09-14 13:45:05 +0200

    registered notes in the database

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,3 +4,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Notes(models.Model):
     user  = models.ForeignKey(User, on_delete= models.CASCADE)
+    tittle = models.CharField(max_length=200)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = "notes"
+        verbose_name_plural = "notes"
+
+    def __str__(self):
+        return self.tittle
